a1b178334752104b81d2e918ec67519a6684466c
Eric Stenseth <eric.stenseth@gmail.com>
2023-11-24 14:35:05 +0100

    Added clap and commandline args

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
+use std::path::PathBuf;
+use clap::Parser;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    path: PathBuf,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
 }
 
 
